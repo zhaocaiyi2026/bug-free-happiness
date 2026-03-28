03001e5cb51cf7792c02b886eb9e5bff98405aa8
user2016546699 <960338758748796-user2016546699@noreply.coze.cn>
2026-03-28 10:18:11 +0800

    chore: init env

---
+++ client/app/_layout.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { LogBox } from 'react-native';
+import Toast from 'react-native-toast-message';
+import { AuthProvider } from "@/contexts/AuthContext";
+import { ColorSchemeProvider } from '@/hooks/useColorScheme';
+
+LogBox.ignoreLogs([
+  "TurboModuleRegistry.getEnforcing(...): 'RNMapsAirModule' could not be found",
+  // 添加其它想暂时忽略的错误或警告信息
+]);
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <ColorSchemeProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <StatusBar style="dark"></StatusBar>
+          <Stack screenOptions={{
+            // 设置所有页面的切换动画为从右侧滑入，适用于iOS 和 Android
+            animation: 'slide_from_right',
+            gestureEnabled: true,
+            gestureDirection: 'horizontal',
+            // 隐藏自带的头部
+            headerShown: false
+          }}>
+            <Stack.Screen name="index" options={{ title: "" }} />
+          </Stack>
+          <Toast />
+        </GestureHandlerRootView>
+      </ColorSchemeProvider>
+    </AuthProvider>
+  );
+}
